fe2fb77fbf0da649754772ea109aa6453d8fcbd4
yukhyohwa <shawnlu91@gmail.com>
2026-01-04 17:09:57 +0800

    chore: update gitignore to exclude sensitive SQL templates

--- app/sql_templates/jump_overseas/user_day_log.sql
+++
@@ -1,8 +0,0 @@
--- 1. Example OP User Log (user_day_log)
--- Description: Query a user's log for a specific day.
-
-SELECT *
-FROM g65002007.daily_game_log -- Replace with actual table name
-WHERE role_id = '{role_id}'
-  AND day = '{day}'
-LIMIT 100;
